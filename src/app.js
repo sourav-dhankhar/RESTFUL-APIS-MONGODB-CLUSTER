24d2838ef17dc4ab401c271551f2cedf349c4832
Sourav <sunnydhankhar12@gmail.com>
2021-05-31 22:14:09 +0530

    First commit

---
+++ src/app.js
@@ -0,0 +1,86 @@
+const express=require("express");
+require("./db/conn");
+
+const Student=require("./models/students");
+
+const app=express();
+const port=process.env.PORT||1234;
+
+app.use(express.json());
+
+//create a new student
+
+app.get("/students",async (req,res)=>{
+    
+
+    try {
+        const userdata=await Student.find();    
+        res.send(userdata);    
+    } catch (error) {
+        res.send(error);
+    }
+
+    
+})
+
+app.get("/students/:id",async (req,res)=>{
+    try {
+        //const _id=req.params;   //req.param gives json object with whatever we have given after /students/:
+        const _id=req.params.id;      //it will give directly a id in text form
+        const userdata=await Student.findById(_id);
+        if(!userdata)
+        {
+            res.status(404).send();
+        }
+        else
+        {
+            res.send(userdata);
+        }
+        res.send(userdata);
+    } catch (e) {
+        res.status(500).send(e);      //500 means internel server error   
+    }
+})
+
+app.post("/students",async(req,res)=>{
+    try{
+        const user=new Student(req.body);
+        const createuser=await user.save();
+        res.status(200).send(createuser);
+    }
+    catch(e)
+    {
+        console.log(e);
+        res.status(400).send(e);
+    }
+    // user.save().then(()=>{
+    //     res.status(201).send(user);
+    // }).catch((e)=>{
+    //     res.status(400).send(e);
+    // })
+   
+})
+
+app.delete("/students/:id",async (req,res)=>{
+    try {
+        const _id=req.params.id;
+        const userd=await Student.findByIdAndDelete(_id);
+        if(!userd)
+        {
+            res.status(400).send();
+        }    
+        else
+        {
+            res.send(userd);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+    
+
+
+})
+
+app.listen(port,()=>{
+    console.log(`coonection is at ${port}`);
+})
